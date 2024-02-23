36cd4228ef5532d08eab3a27fc3ac1cff9a8a113
FISH (boxed like a) <112727486+boxedlikeafish45@users.noreply.github.com>
2024-02-22 21:02:33 -0500

    Add files via upload
    v3 is out! learning more about javascript

---
+++ cpt3/headfoot.js
@@ -0,0 +1,20 @@
+let header = `
+<p><b>Note that this information will not be 100% correct, as no amount of paragraphs can reflect your aggregate personality and life experiences. More things define us than a Homestuck aspect, y'know. It's just one (of many) way(s) to start thinking about what you truly value, what your goals are, and how you can accomplish them.</b></p>
+
+<p>Again, massive props to the research /u/d3wdr0p has done. Their work is featured here in so many ways - they created the aspect wheel above, written the classpect brochure entries below, and done the overall research which informs this test. I truly do value the work they're doing for this esoteric-ass community.</p>
+
+<img src="brochurewheel.png" alt="A wheel showcasing the names of all of the aspects. The wheel shows where your aspect is in relation to other aspects. Breath is at the top-most point, and it is labelled as optimism - mutability. Time is at the right-most point, and it is labelled as catharsis - hot. Blood is at the bottom-most point, and it is labelled as pessimism - immutability. Space is at the left-most point, and it is labelled as conceptual - cold. Read out clockwise, the aspects on the wheel are labeled thus: Breath, Life, Light; Time, Heart, Rage; Blood, Doom, Void; Space, Mind, Hope; and then the wheel loops back around to Breath.">
+<br><small>The Aspect Wheel, featuring all 12 aspects</small>
+`;
+
+let inv = `
+<p><b>Inversion TL;DR</b>
+<br>Alright, this is going to be even <i>more</i> esoteric than what your silly homestuck label is. It's not that founded in Homestuck proper, but I trust the research (and my own experiences) enough to put it here. 
+<br>Inversion is the value that is opposite to your aspect. For example, if your aspect was Time, then the inversion of your aspect would be Space. Basically, your inversion is the box that you felt antagonistic towards in the test you were given.
+<br>Look, if you know what you value, it's just another step towards presuming that what you value is what everyone else or the <i>universe</i> should value. There's merit to the other side, and you shouldn't just presume that <i>your</i> aspect is the way that everything *should* work. An inverted aspect is not all that different from its original - similar circumstances simply led to opposing conclusions.</p>
+`;
+
+document.getElementById("headerInfo").innerHTML = header;
+
+document.getElementById("inversion").innerHTML = inv;
+
